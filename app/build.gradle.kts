becbc154f5b3fd1c2b30942203409a3d12ddfbb4
seeyou-github <165247107+seeyou-github@users.noreply.github.com>
2026-04-14 02:38:48 +0800

    // alias(libs.plugins.google.services)     alias(libs.plugins.firebase.crashlytics)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.ksp)
-    // alias(libs.plugins.google.services)
-    alias(libs.plugins.firebase.crashlytics)
+  
 }
 
 android {
